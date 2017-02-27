c0bd12bd71dbe53dfa8da88c86e9672a355c20ab
Devin Rousso <dcrousso@gmail.com>
2017-02-26 19:04:28 -0800

    Fix style

--- Util.php
+++ Util.php
@@ -1,14 +1,12 @@
 <?php
 
 class Util {
-
-  public static function splitWords($string) {
-    $words = array_filter(preg_split("/\W/", $string));
-    $words = array_count_values($words);
-    arsort($words);
-    
-    return $words;
-  }
+	public static function splitWords($string) {
+		$words = array_filter(preg_split("/\W/", strtolower($string)));
+		$words = array_count_values($words);
+		arsort($words);
+		return $words;
+	}
 }
 
 ?>
