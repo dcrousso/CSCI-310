8e660e33ce45527db15fd53e7fc333cc4f58093b
Devin Rousso <dcrousso@gmail.com>
2017-02-26 17:35:36 -0800

    Implement API::getTrackLyricsGet

--- API.php
+++ API.php
@@ -7,11 +7,10 @@ class API {
 
 	public static function getTrackSearch($artist) {
 		$response = file_get_contents("https://api.musixmatch.com/ws/1.1/track.search?apikey=" . API::$KEY . "&q_artist=" . urlencode($artist) . "&page_size=100");
-		$json = json_decode($response, true)["message"]["body"]["track_list"];
-		return $json;
+		$json = json_decode($response, true)["message"]["body"];
 
 		$result = array();
-		foreach ($json as $item) {
+		foreach ($json["track_list"] as $item) {
 			array_push($result, array(
 				"artist_name" => $item["track"]["artist_name"],
 				"track_id"    => $item["track"]["track_id"],
@@ -23,7 +22,11 @@ class API {
 	}
 
 	public static function getTrackLyricsGet($trackID) {
+		$response = file_get_contents("https://api.musixmatch.com/ws/1.1/track.lyrics.get?apikey=" . API::$KEY . "&track_id=" . $trackID);
+		$json = json_decode($response, true)["message"]["body"];
 
+		$lyrics = $json["lyrics"]["lyrics_body"]);
+		return substr($lyrics, 0, strpos($lyrics, "\n\n...\n\n******* This Lyrics is NOT for Commercial use *******"));
 	}
 
 	public static function getArtistSearch($artist) {
