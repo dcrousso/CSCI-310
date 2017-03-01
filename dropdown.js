4768a91520fb52ede04360d2f6f1be0cd8ad2b4e
Devin Rousso <dcrousso@gmail.com>
2017-02-28 19:59:42 -0800

    Add artist image to dropdown

--- dropdown.js
+++ dropdown.js
@@ -9,11 +9,20 @@ Array.from(document.querySelectorAll("input[type=\"search\"][name=\"a[]\"]")).fo
 			let container = input.insertAdjacentElement("afterEnd", document.createElement("div"));
 			json.forEach(artist => {
 				let item = container.appendChild(document.createElement("div"));
-				item.textContent = artist;
 				item.addEventListener("click", event => {
 					input.value = artist;
 					container.remove();
 				});
+
+				let image = item.appendChild(document.createElement("img"));
+				image.src = "artist_placeholder.png";
+				fetch(`https://www.googleapis.com/customsearch/v1?cx=016332437324068616925:stn7bqajpbc&key=AIzaSyA7CUZ9Lr8l7NS761Y2XUiWMCBxo5rCraM&q=${artist}`)
+				.then(response => response.json())
+				.then(json => {
+					image.src = json["items"][0]["pagemap"]["imageobject"][0]["url"];
+				});
+
+				item.appendChild(document.createTextNode(artist));
 			});
 		});
 	});
