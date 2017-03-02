89aaf2f062f1a4f26c61fa866386224381f2e5bb
Devin Rousso <dcrousso@gmail.com>
2017-03-02 04:42:37 -0800

    Ignore common words

--- Util.php
+++ Util.php
@@ -9,7 +9,13 @@ class Util {
 	 */
 	public static function splitWords($string) {
 		// lowercase all words in string, then split w RegEx, then filter out white spaces
-		$words = array_filter(preg_split("/\W/", strtolower($string)));
+		$words = array_filter(preg_split("/\W/", strtolower($string)), function($item) {
+			if (!$item || !strlen($item))
+				return false;
+
+			// NLTK's list of english stopwords (https://gist.github.com/sebleier/554280)
+			return !in_array($item, array("i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your", "yours", "yourself", "yourselves", "he", "him", "his", "himself", "she", "her", "hers", "herself", "it", "its", "itself", "they", "them", "their", "theirs", "themselves", "what", "which", "who", "whom", "this", "that", "these", "those", "am", "is", "are", "was", "were", "be", "been", "being", "have", "has", "had", "having", "do", "does", "did", "doing", "a", "an", "the", "and", "but", "if", "or", "because", "as", "until", "while", "of", "at", "by", "for", "with", "about", "against", "between", "into", "through", "during", "before", "after", "above", "below", "to", "from", "up", "down", "in", "out", "on", "off", "over", "under", "again", "further", "then", "once", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each", "few", "more", "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than", "too", "very", "s", "t", "can", "will", "just", "don", "should", "now"));
+		});
 
 		// count the number of occurrences
 		$words = array_count_values($words);
