2de369a91d6d25ee6bd3bba1c12e7608f05b6ac7
CliffLee <clifford.lee1997@gmail.com>
2017-03-04 16:37:54 -0800

    Removed previous behat test testing

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -21,35 +21,4 @@ class FeatureContext implements Context
     {
     }
 
-    /**
-     * @Given there is a(n) :arg1, which costs $:arg2
-     */
-    public function thereIsAWhichCostsPs($arg1, $arg2)
-    {
-      throw new PendingException();
-    }
-
-    /**
-     * @When I add the :arg1 to the basket
-     */
-    public function iAddTheToTheBasket($arg1)
-    {
-        throw new PendingException();
-    }
-
-    /**
-     * @Then I should have :arg1 product in the basket
-     */
-    public function iShouldHaveProductInTheBasket($arg1)
-    {
-        throw new PendingException();
-    }
-
-    /**
-     * @Then the overall basket price should be $:arg1
-     */
-    public function theOverallBasketPriceShouldBe($arg1)
-    {
-        throw new PendingException();
-    }
 }
