68418e183a41fda3b088f818f440d84582fc4324
CliffLee <clifford.lee1997@gmail.com>
2017-03-04 16:38:19 -0800

    searchingForArtistRedirectsToArtistPage test initial commit

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -22,34 +22,9 @@ class FeatureContext implements Context
     }
 
     /**
-     * @Given there is a(n) :arg1, which costs $:arg2
+     * @Given I am on :arg1 
      */
-    public function thereIsAWhichCostsPs($arg1, $arg2)
-    {
+    public function isOnSpecificPage($arg1) {
       throw new PendingException();
-    }
-
-    /**
-     * @When I add the :arg1 to the basket
-     */
-    public function iAddTheToTheBasket($arg1)
-    {
-        throw new PendingException();
-    }
-
-    /**
-     * @Then I should have :arg1 product in the basket
-     */
-    public function iShouldHaveProductInTheBasket($arg1)
-    {
-        throw new PendingException();
-    }
-
-    /**
-     * @Then the overall basket price should be $:arg1
-     */
-    public function theOverallBasketPriceShouldBe($arg1)
-    {
-        throw new PendingException();
     }
 }
