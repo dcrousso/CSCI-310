c6af00ac3a022095d4518b9bc373fa201b84c2ef
CliffLee <clifford.lee1997@gmail.com>
2017-03-29 21:37:55 -0700

    Scrum 4 - Planned out white box tests Scrum 4 - Planned out two sets for API (ACM and IEEE) Scrum 4 - included whitebox tests in a test/ dir

---
+++ Project 2/tests/API_ACM_Test.php
@@ -0,0 +1,62 @@
+<?php
+
+require("vendor/autoload.php");
+
+require_once("API_ACM.php");
+
+class API_ACM_Test extends PHPUnit\Framework\TestCase {
+	public static function setUpBeforeClass() {
+		echo "=== API_ACM ===" . "\n";
+	}
+
+	protected function setUp() {
+		echo $this->getName();
+	}
+
+	protected function tearDown() {
+		echo "\n";
+	}
+
+	public static function tearDownAfterClass() {
+		echo "=== API_ACM ===" . "\n\n";
+	}
+
+	// API_ACM::query
+
+	public function testQueryWithNonStringShouldReturnEmptyArray() {
+		$result = API_ACM::query(NULL);
+
+		$this->assertTrue(is_array($result));
+		$this->assertCount(0, $result);
+	}
+
+	public function testQueryWithEmptyStringShouldReturnEmptyArray() {
+		$result = API_ACM::query("");
+
+		$this->assertTrue(is_array($result));
+		$this->assertCount(0, $result);
+	}
+
+	public function testQueryWithInvalidStringShouldReturnEmptyArray() {
+		$result = API_ACM::query("abcdefghijklmnopqrstuvxwyz");
+
+		$this->assertTrue(is_array($result));
+		$this->assertCount(0, $result);
+	}
+
+	public function testQueryWithValidStringShouldReturnValidArray() {
+		$result = API_ACM::query("PHP");
+
+		$this->assertTrue(is_array($result));
+		$this->assertGreaterThan(0, count($result));
+
+		$this->assertGreaterThan(0, strlen($result[0]["title"]));
+
+		$this->assertGreaterThan(0, count($result[0]["authors"]));
+		$this->assertGreaterThan(0, strlen($result[0]["authors"][0]));
+
+		$this->assertGreaterThan(0, strlen($result[0]["conference"]));
+
+		$this->assertGreaterThan(0, strlen($result[0]["pdf"]));
+	}
+}
---
+++ Project 2/tests/API_IEEE_Test.php
@@ -0,0 +1,62 @@
+<?php
+
+require("vendor/autoload.php");
+
+require_once("API_IEEE.php");
+
+class API_IEEE_Test extends PHPUnit\Framework\TestCase {
+	public static function setUpBeforeClass() {
+		echo "=== API_IEEE ===" . "\n";
+	}
+
+	protected function setUp() {
+		echo $this->getName();
+	}
+
+	protected function tearDown() {
+		echo "\n";
+	}
+
+	public static function tearDownAfterClass() {
+		echo "=== API_IEEE ===" . "\n\n";
+	}
+
+	// API_IEEE::queryText
+
+	public function testQueryTextWithNonStringShouldReturnEmptyArray() {
+		$result = API_IEEE::queryText(NULL);
+
+		$this->assertTrue(is_array($result));
+		$this->assertCount(0, $result);
+	}
+
+	public function testQueryTextWithEmptyStringShouldReturnEmptyArray() {
+		$result = API_IEEE::queryText("");
+
+		$this->assertTrue(is_array($result));
+		$this->assertCount(0, $result);
+	}
+
+	public function testQueryTextWithInvalidStringShouldReturnEmptyArray() {
+		$result = API_IEEE::queryText("abcdefghijklmnopqrstuvxwyz");
+
+		$this->assertTrue(is_array($result));
+		$this->assertCount(0, $result);
+	}
+
+	public function testQueryTextWithValidStringShouldReturnValidArray() {
+		$result = API_IEEE::queryText("PHP");
+
+		$this->assertTrue(is_array($result));
+		$this->assertGreaterThan(0, count($result));
+
+		$this->assertGreaterThan(0, strlen($result[0]["title"]));
+
+		$this->assertGreaterThan(0, count($result[0]["authors"]));
+		$this->assertGreaterThan(0, strlen($result[0]["authors"][0]));
+
+		$this->assertGreaterThan(0, strlen($result[0]["conference"]));
+
+		$this->assertGreaterThan(0, strlen($result[0]["pdf"]));
+	}
+}
