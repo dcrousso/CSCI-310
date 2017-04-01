ecffe01fbdbaf851ad1857a0295c238b8378ad71
CliffLee <clifford.lee1997@gmail.com>
2017-03-31 20:12:20 -0700

    scrum 5 - initial API_IEEE planning scrum 5 - API_IEEE endpoint stubs scrum 5 - Completed IEEE endpoint functionality

---
+++ Project 2/API_IEEE.php
@@ -0,0 +1,36 @@
+<?php
+
+class API_IEEE {
+	private static $URL = "http://ieeexplore.ieee.org/gateway/ipsSearch.jsp?";
+
+	public static function queryText($query, $count = 10) {
+		if (!is_string($query) || !strlen($query))
+			return array();
+
+		if (!is_numeric($count) || $count <= 0)
+			return array();
+
+		$response = file_get_contents(API_IEEE::$URL . "&querytext=" . $query . "&hc=" . $count);
+
+		$xml = simplexml_load_string($response, "SimpleXMLElement", LIBXML_NOCDATA);
+		if (!$xml)
+			return array();
+
+		$json = json_decode(json_encode($xml), TRUE);
+		if (!$json || !isset($json["document"]))
+			return array();
+		
+		return array_map(function($item) {
+			return array(
+				"title"      => $item["title"],
+				"authors"    => preg_split("/;\s*/", $item["authors"]),
+				"conference" => $item["pubtitle"],
+				"abstract"   => $item["abstract"],
+				"pdf"        => $item["pdf"]
+			);
+		},  $json["document"]);
+	}
+}
+
+?>
+
