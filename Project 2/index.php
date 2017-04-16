501b17d481118b4bb7ef34e125b8a16e14befac8
Devin Rousso <code@devinrousso.com>
2017-04-16 13:29:06 -0700

    Fix styling

--- Project 2/index.php
+++ Project 2/index.php
@@ -1,23 +1,22 @@
 <!DOCTYPE html>
 <html>
-<head>
-	<link rel="stylesheet" href="common.css">
-	<style>
-		body {
-			text-align: center;
-			margin-top: 10%;
-		}
-
-	</style>
-</head>
-<body>
-	<main>
-		<img src="Cloudify-logo-w-text.png" width=400>
-		<form action="cloud.php">
-		<input name="q" type="search" placeholder="Keywords, Authors, Conferences" style="width:30%;" autofocus required>
-			<input name="n" type="number" min="10" placeholder="Top X Papers" required>
-			<button>Search</button>
-		</form>
-	</main>
-</body>
+	<head>
+		<link rel="stylesheet" href="common.css">
+		<style>
+body {
+	text-align: center;
+	margin-top: 10%;
+}
+		</style>
+	</head>
+	<body>
+		<main>
+			<img src="Cloudify-logo-w-text.png" width=400>
+			<form action="cloud.php">
+				<input name="q" type="search" placeholder="Search" autofocus required>
+				<input name="n" type="number" placeholder="Count" min="10" value="10" required>
+				<button>Search</button>
+			</form>
+		</main>
+	</body>
 </html>
